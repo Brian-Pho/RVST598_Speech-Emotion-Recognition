be92bb05a1002cb684f682cc9685ed6e201bff2b
Brian <brianpho7@gmail.com>
2019-06-18 12:05:33 -0600

    Update Ravdess code with caching ability

--- src/database_formatter/db_constants.py
+++ src/database_formatter/db_constants.py
@@ -1,6 +1,12 @@
 import os
 
-DB_PATH = os.path.join("..", "..", "data")
+DB_PATH = r"C:\Users\BT_Lab\Documents\RVST598_Speech-Emotion-Recognition\data"
 CREMAD_DB_PATH = os.path.join(DB_PATH, "crema-d")
+
 IEMOCAP_DB_PATH = os.path.join(DB_PATH, "iemocap")
+
 RAVDESS_DB_PATH = os.path.join(DB_PATH, "ravdess")
+RAVDESS_SAMPLES_CACHE_PATH = os.path.join(
+    RAVDESS_DB_PATH, "ravdess_samples_cache.npy")
+RAVDESS_LABELS_CACHE_PATH = os.path.join(
+    RAVDESS_DB_PATH, "ravdess_labels_cache.npy")
--- src/database_formatter/ravdess.py
+++ src/database_formatter/ravdess.py
@@ -2,30 +2,68 @@ import os
 
 import librosa
 import numpy as np
-from db_constants import RAVDESS_DB_PATH
+from db_constants import (
+    RAVDESS_DB_PATH, RAVDESS_SAMPLES_CACHE_PATH, RAVDESS_LABELS_CACHE_PATH)
 from src import constants as c
 
 NUM_ACTORS = 24
+RAVDESS_EMOTION_MAP = {
+    "01": c.NEU,
+    "02": c.NEU,  # Map calm to neutral
+    "03": c.HAP,
+    "04": c.SAD,
+    "05": c.ANG,
+    "06": c.FEA,
+    "07": c.DIS,
+    "08": c.SUR,
+}
 
 
 def load_data():
     """
+    Loads the RAVDESS database from the cached files. If they don't exist,
+    then read the database and create the cache.
+
+    :return: Tuple of (samples, labels) or None if unsuccessful.
+    """
+    ravdess_samples = None
+    ravdess_labels = None
+
+    try:
+        # Attempt to read the cache
+        ravdess_samples = np.load(RAVDESS_SAMPLES_CACHE_PATH, allow_pickle=True)
+        ravdess_labels = np.load(RAVDESS_LABELS_CACHE_PATH, allow_pickle=True)
+        print("Successfully loaded the RAVDESS cache.")
+
+    except IOError as e:
+        # Since the cache doesn't exist, create it.
+        ravdess_samples, ravdess_labels = read_data()
+        np.save(RAVDESS_SAMPLES_CACHE_PATH, ravdess_samples, allow_pickle=True)
+        np.save(RAVDESS_LABELS_CACHE_PATH, ravdess_labels, allow_pickle=True)
+        print("Successfully cached the RAVDESS database.")
+
+    finally:
+        ravdess_data = (ravdess_samples, ravdess_labels)
+        return ravdess_data
+
+
+def read_data():
+    """
     Reads the RAVDESS database into tensors.
 
     Sample output:
-        [
-            ((data, fs), label),
-            ((array([0.0000000e+00, ...,], dtype=float32), 22050), 6),
-            ...
-        ]
+        (array([[array([ 1.5591205e-07, -1.5845627e-07,  1.5362870e-07, ...,
+        0.0000000e+00,  0.0000000e+00,  0.0000000e+00], dtype=float32),
+        22050]], dtype=object), array([0, 0, 0, ..., 6]))
 
-    :return: Tensor of shape (60, 2) where 60 is the number of samples and 2 is
-             the sample and label.
+    :return: Tuple of (samples, labels) where the samples are a tensor of shape
+             (files, data), and the labels are an array of integers. The data is
+             formatted as (audio time series, sampling rate).
     """
     samples = []
     labels = []
     # for actor in range(1, NUM_ACTORS + 1):
-    for actor in range(1, 10):
+    for actor in range(1, 2):
         actor_foldername = "Actor_{:02d}".format(actor)
         actor_path = os.path.join(RAVDESS_DB_PATH, actor_foldername)
 
@@ -33,7 +71,7 @@ def load_data():
             sample_path = os.path.join(actor_path, sample_filename)
 
             # Read the sample
-            print(sample_filename)
+            # print(sample_filename)
             samples.append(librosa.load(sample_path))
 
             # Read the label
@@ -44,35 +82,26 @@ def load_data():
 
 def _interpret_label(filename):
     """
-    Given a filename, it returns a tensor representing the emotion label of the
-    file/sample.
+    Given a filename, it returns an integer representing the emotion label of
+    the file/sample.
     :return: Integer
     """
-    RAVDESS_EMOTION_MAP = {
-        "01": c.NEU,
-        "02": c.NEU,
-        "03": c.HAP,
-        "04": c.SAD,
-        "05": c.ANG,
-        "06": c.FEA,
-        "07": c.DIS,
-        "08": c.SUR,
-    }
-
     # Parse emotion ID from filename. It's the third number from the left
     # according to https://zenodo.org/record/1188976.
     emotion_id = filename.split("-")[2]
     emotion = RAVDESS_EMOTION_MAP[emotion_id]
 
     # Return a new emotion ID that's standardized across databases.
-    # print(emotion, c.EMOTION_MAP[emotion])
     return c.EMOTION_MAP[emotion]
 
 
 def main():
     ravdess_samples, ravdess_labels = load_data()
     print(ravdess_samples.shape)
+    # print(ravdess_samples[0][0])  # Amplitude data
+    # print(ravdess_samples[0][1])  # Sampling rate data
     print(ravdess_labels.shape)
+    # print(ravdess_labels[0])
     # print(ravdess_data)
     # print(ravdess_data[0][1])
     # for data in ravdess_data:
