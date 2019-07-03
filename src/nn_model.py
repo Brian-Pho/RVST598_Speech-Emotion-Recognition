807891fc61115ba5605133da29768c2af3f3ba65
Brian <brianpho7@gmail.com>
2019-07-03 15:31:13 -0600

    Update NN model

--- src/nn_model.py
+++ src/nn_model.py
@@ -15,18 +15,22 @@ def build_model():
     """
     model = models.Sequential()
     # kernel_regularizer=regularizers.l2(0.001),
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(200, 163, 1)))
+    model.add(layers.Conv2D(32, (5, 5), activation='relu', input_shape=(200, 249, 1)))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
     model.add(layers.MaxPooling2D((2, 2)))
     model.add(layers.Conv2D(64, (3, 3), activation='relu'))
     model.add(layers.Flatten())
     model.add(layers.Dense(64, activation='relu'))
-    # model.add(layers.Dropout(0.5))
+    model.add(layers.Dropout(0.5))
     model.add(layers.Dense(32, activation='relu'))
-    # model.add(layers.Dropout(0.5))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(16, activation='relu'))
+    model.add(layers.Dropout(0.5))
     model.add(layers.Dense(7, activation='softmax'))
-    # model.add(layers.Dropout(0.5))
+    model.add(layers.Dropout(0.5))
     model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
