3f49e7120821fcdfa24178843a66a39df0d20f92
Brian <brianpho7@gmail.com>
2019-07-09 10:37:25 -0600

    Minor updates to documentation and scratch

--- src/scratch.py
+++ src/scratch.py
@@ -68,3 +68,32 @@ in this file aren't runnable but should help debug or view data.
 # plt.pcolormesh(melspecgram, cmap=plt.get_cmap("YlGnBu"))
 # plt.colorbar(melspecgram)
 # plt.show()
+
+# import numpy as np
+# from keras.models import Sequential
+# from keras.layers import Dense, Dropout
+#
+# # Generate dummy data
+# x_train = np.random.random((1000, 20))
+# y_train = np.random.randint(3, size=(1000, 1))
+# x_test = np.random.random((100, 20))
+# y_test = np.random.randint(3, size=(100, 1))
+#
+# model = Sequential()
+# model.add(Dense(64, input_dim=20, activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(64, activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(1, activation='sigmoid'))
+#
+# model.compile(loss='binary_crossentropy',
+#               optimizer='rmsprop',
+#               metrics=['accuracy'])
+#
+# model.fit(x_train, y_train,
+#           epochs=20,
+#           batch_size=128)
+# score = model.evaluate(x_test, y_test, batch_size=128)
+# print(score)
+# prediction = model.predict(x_test[0:5])
+# print(prediction, y_test[0:5])
